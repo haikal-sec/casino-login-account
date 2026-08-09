26d75ff13c7ab2cd262b5d5ef9650f5b8f4c3e77
ghost <m.haikalshamsuddin@gmail.com>
2026-08-10 06:42:18 +0800

    Add files via upload

---
+++ acc-casino.py
@@ -0,0 +1,21 @@
+# haikal-sec ( make this at 5am *cuz i cant sleep lol)
+import time
+import getpass
+from art import tprint
+
+tprint('NPC CASINO!')
+urname = input('What is your name: ')
+print(f'Nice to meet you {urname}!')
+username = input('Enter your username: ')
+password = getpass.getpass('Enter password: ')
+print('Processing Verified...')
+time.sleep(5)
+
+if username == 'npc123' and password == 'notnpc123':
+    print('Access Granted!')
+    bankAccount = input('How many you wanna to topup: ')
+    gambling = input('How many you wanna gamble: ')
+    balanceAccount = int(bankAccount) - int(gambling)
+    print(f'Loser! your current balance: {balanceAccount}')
+else: 
+    print('Access Denied!')
